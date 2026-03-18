50fa7bc8d2decefd9713908b520596214a530505
aarohi001g <aarohi.25b01010076@abes.ac.in>
2026-03-18 23:36:26 +0530

    exp4updated

--- exp4/main.jsx
+++ exp4/main.jsx
@@ -1,23 +1,37 @@
-function calculateGrade(score) {
-    if (score >= 90) {
-        return 'A'; 
-    } else if (score >= 80) {
-        return 'B'; 
-    }   else if (score >= 70) {     
-        return 'C'; 
-    } else if (score >= 60) {
-        return 'D'; 
-    } else {
-        return 'F'; 
-    }       
-
-    document.getElementById('result').innerHTML=
-    "totalmarks" + totalmarks 
-
-
-
-
-
-
-
-
+function calculateResult(){
+let n = document.getElementById("subjects").value;
+let total = 0;
+for(let i=1;i<=n;i++){
+let marks = parseFloat(prompt("Enter marks for Subject " + i));
+total = total + marks;
+}
+let average = total / n;
+let grade;
+let result;
+if(average >= 90){
+grade = "A+";
+}
+else if(average >= 75){
+grade = "A";
+}
+else if(average >= 60){
+grade = "B";
+}
+else if(average >= 50){
+grade = "C";
+}
+else{
+grade = "F";
+}
+if(average >= 40){
+result = "PASS";
+}
+else{
+result = "FAIL";
+}
+document.getElementById("result").innerHTML =
+"Total Marks: " + total + "<br>" +
+"Average Marks: " + average.toFixed(2) + "<br>" +
+"Grade: " + grade + "<br>" +
+"Result: " + result;
+}
